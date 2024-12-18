0f770542f05149b3706066a8e16e03f38aab8b23
unknown <rosa.angel.daniel@gmail.com>
2024-12-18 16:54:05 -0700

    correctly initialized the project

--- server/server.js
+++
@@ -1,8 +0,0 @@
-import Stripe from "stripe";
-import 'dotenv/config';
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || "", {
-    apiVersion: '2024-11-20.acacia',
-});
-const cust = await stripe.customers.list();
-console.log(cust);
-//# sourceMappingURL=server.js.map
